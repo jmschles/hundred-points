9e1f6660a22d61ba56381ef67892bd33f2b2db53
Jamie Schlessinger <james.schlessinger@gmail.com>
2020-03-27 18:52:22 -0700

    More deploy stuff

--- config/prod.exs
+++ config/prod.exs
@@ -18,6 +18,7 @@ import_config "prod.secret.exs"
 config :hundred_points, HundredPointsWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  check_origin: ["//*.gigalixirapp.com", "http://localhost:4000"],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
